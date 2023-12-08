0d69995a61e86723aa3b9884a613f9d0a74b09ad
Emeleb45 <145105762+Emeleb45@users.noreply.github.com>
2023-12-08 10:17:26 +0100

    bigp

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 let ischanged = false
 let vidpath = ""
+let lastPosition = null;
 function playSound() {
   var boom = new Audio('sounds/Boom.mp3')
   boom.volume = 1.0
@@ -46,14 +47,27 @@ function stopVideo() {
   videoPath = ""
 }
 
-function changeImage() {
-  var imgElement = document.getElementById("phoneimg")
-  if (ischanged === false) {
-    imgElement.src = "imgs/iphoneempty.png"
-    ischanged = true
-  }
-  else {
-    imgElement.src = "imgs/iphone.png"
-    ischanged = false
+function getRandomPosition() {
+  let radius = 50
+  const minX = lastPosition ? Math.max(0, lastPosition.x - radius) : 0
+  const maxX = lastPosition ? Math.min(window.innerWidth - 200, lastPosition.x + radius) : window.innerWidth - 200
+  const minY = lastPosition ? Math.max(0, lastPosition.y - radius) : 0
+  const maxY = lastPosition ? Math.min(window.innerHeight - 60, lastPosition.y + radius) : window.innerHeight - 60
+  const randomX = Math.random() * (maxX - minX) + minX
+  const randomY = Math.random() * (maxY - minY) + minY
+  return { x: randomX, y: randomY };
+}
+
+function moveButton(buttonId) {
+  let button = document.getElementById(buttonId)
+  if (lastPosition == null)  {
+  let centerX = window.innerWidth / 500
+  let centerY = window.innerHeight / 500
+  lastPosition = { x: centerX, y: centerY }
+  button.style.transform = `translate3d(${centerX}px, ${centerY}px, 0)`
+  } else {
+    let newPosition = getRandomPosition(lastPosition)
+    lastPosition = newPosition
+    button.style.transform = `translate3d(${newPosition.x}px, ${newPosition.y}px, 0)`
   }
 }
